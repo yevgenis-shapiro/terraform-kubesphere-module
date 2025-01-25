bf7b88c472e615145f1b2d502b20ded7e67777b2
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-01-26 01:53:48 +0200

    update

--- modules/nginx/main.tf
+++ modules/nginx/main.tf
@@ -1,5 +1,3 @@
-
-
 resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
@@ -36,3 +34,20 @@ EOT
 
 }
 
+resource "null_resource" "wait_for_ingress_nginx" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\nWaiting for the nginx ingress controller...\n"
+      kubectl wait --namespace ${helm_release.ingress_nginx.namespace} \
+        --for=condition=ready pod \
+        --selector=app.kubernetes.io/component=controller \
+        --timeout=90s
+    EOF
+  }
+
+  depends_on = [helm_release.ingress_nginx]
+}
