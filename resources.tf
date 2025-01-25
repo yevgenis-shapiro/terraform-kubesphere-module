42ef0d021addd38cfa506c174cc830ed4ed5427b
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-01-26 01:05:41 +0200

    update

--- resources.tf
+++ resources.tf
@@ -6,18 +6,16 @@ data "external" "subnet" {
   ]
 }
 
-
 module "metallb" {
   source = "./modules/metallb"
-
+  depends_on = [kind_cluster.default]
   #version  = var.metallb_helm_version
 
 }
 
-
 module "nginx" {
   source = "./modules/nginx"
-
+  depends_on = [kind_cluster.default]
   #ingress_nginx_helm_version = var.ingress_nginx_helm_version
   #ingress_nginx_namespace    = var.ingress_nginx_namespace
 
