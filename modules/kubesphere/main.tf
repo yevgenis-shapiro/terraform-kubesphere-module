bacd683dadda122813307a555ea4499a1b5c2f6b
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-01-29 21:46:32 +0200

    update

--- modules/kubesphere/main.tf
+++ modules/kubesphere/main.tf
@@ -1,41 +1,13 @@
+resource "helm_release" "ks_installer" {
+  name       = "ks-installer"
+  namespace  = "kubesphere-system"
+  repository = "https://charts.kubesphere.io/main"
+  chart      = "ks-installer"
+  version    = "4.1.2"
 
-resource "helm_release" "kubesphere" {
-  name             = "kubesphere"
-  repository       = "https://charts.kubesphere.io/main"
-  chart            = "ks-core"
-  namespace        = "kubesphere-system"
   create_namespace = true
-  force_update     = true
-  wait             = true
-  timeout          = 300
-  
 
   values = [
-    <<EOT
-
-  serviceMonitor:
-    enabled: false
-  jmx:
-    enabled: true
-
-EOT
+    "${file("config/ks-installer-values.yaml")}"
   ]
-}
-
-resource "null_resource" "wait_for_kafka" {
-  triggers = {
-    key = uuid()
-  }
-
-  provisioner "local-exec" {
-    command = <<EOF
-      printf "\nWaiting for the kafka pods to start...\n"
-      sleep 5
-      until kubectl wait -n ${helm_release.kafka.namespace} --for=condition=Ready pods --all; do
-        sleep 2
-      done  2>/dev/null
-    EOF
-  }
-
-  depends_on = [helm_release.kafka]
 }
