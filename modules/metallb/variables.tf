67422f5ac624853acb86be58c6df09547fe3febd
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-01-31 16:57:19 +0200

    update

--- modules/metallb/variables.tf
+++ modules/metallb/variables.tf
@@ -1,6 +1,11 @@
-
 variable "kind_cluster_config_path" {
   type        = string
   description = "The location where this cluster's kubeconfig will be saved to."
   default     = "~/.kube/config"
+}
+
+variable "metallb_ip_range" {
+  description = "The kind version of kubernetes."
+  default     = "172.18.255.1-172.18.255.250"
+  type        = string
 }
